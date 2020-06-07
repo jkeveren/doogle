4360fd917b366e30bb0e38ca00c9394d6b451b34
James Keveren <james@keve.ren>
2020-06-07 18:15:57 +0200

    Revert "More better"
    This reverts commit fbb1094c8ad6a3fb2f08729af642a461b6725aee.

--- index.mjs
+++ index.mjs
@@ -1,90 +1,52 @@
 import http from 'http';
 import https from 'https';
-import path from 'path';
-import url from 'url';
-import {promises as fs} from 'fs';
 
 const server = http.createServer();
 
-const deployedHostname = 'localhost:50000';
-
-const googleTopHostname = 'google.co.uk';
-const googleHostname = 'www.' + googleTopHostname;
-const staticRoot = path.join(url.fileURLToPath(import.meta.url), '../static');
+const googleHostname = 'www.google.co.uk';
 
 server.on('request', async (clientRequest, proxyResponse) => {
 	try {
-		// look for override file
-		let file = null;
-		try {
-			file = await fs.readFile(path.join(staticRoot, clientRequest.url));
-		} catch (error) {
-			if (!['ENOENT', 'EISDIR'].includes(error.code)) {
-				console.error(error);
-			}
-		}
-		if (file) {
-			// send override file
-			proxyResponse.write(file);
-		} else {
-			const URLObject = new url.URL(clientRequest.url, 'https://' + clientRequest.headers.host);
-			console.log(URLObject);
-			if (['/search', '/imghp'].includes(URLObject.pathname)) {
-				URLObject.searchParams.set('q', 'doogle');
-			}
+		// make request to server
+		await new Promise((resolve, reject) => {
+			const options = {
+				hostname: googleHostname,
+				headers: Object.assign(clientRequest.headers, {
+					host: googleHostname
+				}),
+				method: clientRequest.method,
+				path: clientRequest.url,
+				setHost: false,
+				// rejectUnauthorized: false
+			};
 			// make request to server
-			await new Promise((resolve, reject) => {
-				const clientRequestHeadersCopy = Object.assign({}, clientRequest.headers);
-				clientRequestHeadersCopy.host = googleHostname;
-				clientRequestHeadersCopy['accept-encoding'] = 'identity';
-				const options = {
-					headers: clientRequestHeadersCopy,
-					method: clientRequest.method,
-					setHost: false,
-				};
-				// make request to server
-				const proxyRequest = https.request(URLObject, options);
-				proxyRequest.on('error', reject);
-				proxyRequest.on('response', async serverResponse => {
-					try {
-						// sync headers and statusCode
-						for (const header of Object.entries(serverResponse.headers)) {
-							proxyResponse.setHeader(...header);
-						}
-						proxyResponse.statusCode = serverResponse.statusCode;
-						// stream response to client
-						const responseContentType = serverResponse.headers['content-type'];
-						if (responseContentType && ['html'].some(type => responseContentType.includes(type))) {
-							let chunks = [];
-							serverResponse.on('readable', () => {
-								let chunk;
-								while(chunk = serverResponse.read()) {
-									chunks.push(chunk);
-								}
-							});
-							serverResponse.on('end', () => {
-								let body = Buffer.concat(chunks);
-								body = body.toString();
-								body = body.replace(/\.google\.[^/"> ]{0,10}/ig, '.' + clientRequest.headers.host);
-								body = body.replace(/google/ig, 'Doogle');
-								proxyResponse.write(body);
-								resolve();
-							});
-						} else {
-							serverResponse.pipe(proxyResponse);
-							serverResponse.on('end', resolve);
-						}
-					} catch (error) {
-						reject(error);
+			const proxyRequest = https.request(options);
+			proxyRequest.on('error', reject);
+			proxyRequest.on('response', async serverResponse => {
+				try {
+					// sync headers and statusCode
+					for (const header of Object.entries(serverResponse.headers)) {
+						proxyResponse.setHeader(...header);
 					}
-				});
-				clientRequest.pipe(proxyRequest);
-				proxyRequest.end();
+					proxyResponse.statusCode = serverResponse.statusCode;
+					// stream response to client
+					serverResponse.on('readable', () => {
+						let chunk;
+						while(chunk = serverResponse.read()) {
+							proxyResponse.write(chunk);
+						}
+					});
+					serverResponse.on('end', resolve);
+				} catch (error) {
+					reject(error);
+				}
 			});
-		}
+			clientRequest.pipe(proxyRequest);
+			proxyRequest.end();
+		});
 	} catch (error) {
 		proxyResponse.statusCode = 500;
-		proxyResponse.write('Oops, this bit doesn\'t work.');
+		proxyResponse.write(error.stack);
 		console.error(error);
 	} finally {
 		proxyResponse.end();
